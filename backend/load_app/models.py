1ce58a37c27f232ce7e816343387893bb7243064
macbook <nikitaobolonkov@mail.ru>
2024-12-22 21:30:50 +0300

    [init]

--- backend/load_app/models.py
+++ backend/load_app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-# migrations
 
 class UserRecord(models.Model):
     # TODO: Минимальный набор полей для работы приложения, при желание расширить
-    auth_uuid = models.CharField(max_length=100, unique=True)
-    tg_id = models.CharField(max_length=100, null=True, blank=True)
+    auth_uuid = models.CharField(max_length=100, primary_key=True)
+    tg_id = models.BigIntegerField(unique=True, null=True, blank=True)
     username = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.auth_uuid}: {self.username if self.username is not None else self.tg_id}"
+
